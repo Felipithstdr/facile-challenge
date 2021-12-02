ef6018e507febded47fce0266ceb3245fe602507
Felipi Thiesen Tidra <felipiths58@gmail.com>
2021-12-02 20:36:26 -0300

    facile_challenge

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ app.use(routes);
 //Para rodar localmente, remova o comentário da linha abaixo, e comente a linha subsequente;
 //app.listen(3333);
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });

05a3251b8ebee69bbd926c106a46729f7a972247
Felipi Thiesen Tidra <felipiths58@gmail.com>
2021-12-02 18:12:58 -0300

    facile_challenge

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ const app = express();
 app.use (express.json());
 app.use(routes);
 
-app.listen(3333);
+
+# use alternate localhost and the port Heroku assigns to $PORT
+
+const port = process.env.PORT || 5000;
+
+//app.listen(3333);
+app.listen(PORT, () => {
+	console.log("Server started......." + PORT);
+});

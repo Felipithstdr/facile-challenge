d0682c0b0df61e3d232963c96c5525e81ccdc11e
Felipi Thiesen Tidra <felipiths58@gmail.com>
2021-12-02 20:45:59 -0300

    facile_challenge

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ const app = express();
 app.use (express.json());
 app.use(routes);
 
-//Para rodar localmente, remova o comentário da linha abaixo, e comente a linha subsequente;
-//app.listen(3333);
 
-const PORT = process.env.PORT || 3000;
+app.listen(3333);
+
+//Para rodar no servidor heroku, remova o comentário da linha abaixo, e comente a linha acima;
+/*const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
-});
+});*/
